b627485a8ae11e1e75c0616367ce71265f6fe7f9
arthur neuman <arthur.neuman@gmail.com>
2016-02-25 21:00:05 -0500

    Week 5

---
+++ week_06/src/com/arthurneuman/Arithmetic.java
@@ -0,0 +1,15 @@
+package com.arthurneuman;
+
+
+public class Arithmetic {
+    public static double average(double[] values) throws Exception {
+        if (values.length == 0 ) {
+            throw new Exception("Cannot divide by zero.");
+        }
+        double average = 0;
+        for (double value: values) {
+            average += value;
+        }
+        return average / values.length;
+    }
+}
---
+++ week_06/src/com/arthurneuman/Main.java
@@ -0,0 +1,10 @@
+package com.arthurneuman;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        double[] someValues = {};
+        double averageValue = Arithmetic.average(someValues);
+        System.out.println(averageValue);
+    }
+}
